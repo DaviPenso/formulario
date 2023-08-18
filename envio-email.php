c099e9946e7d3f02b9b7b1b2d1d8fca773cd5f32
Felipe Campelo <me@thearch.tech>
2023-08-18 09:07:24 -0300

    Atualizacoes diversas

--- envio-email.php
+++ envio-email.php
@@ -1,36 +1,91 @@
 <?php
 
-$nome = "Felipe";
+include "MySQL.php";
 
-if ($nome === "Felipe") {
-    $response = [
-        "nomeCompleto" => "Felipe Fernandes Campelo",
-        "idade" => 35,
-        "sexo" => "Masculino",
-        "endereco" => [
-            "logradouro" => "Rodovia Municipal dos Andradas"
-        ],
-    ];
+$connData = [
+    "dbserver" => "localhost",
+    "username" => "root",
+    "password" => "",
+    "database" => "aula_php_email",
+];
 
-    // for ($i = 0; $i <= 10; $i++) {
-    //     echo $i;
-    // }
+$remetenteNome = "Felipe Campelo";
+$remetenteEmail = "felipefcamp@gmail.com";
+$destinatarioNome = $_POST['nome'];
+$destinatarioEmail = $_POST['email'];
+$assunto = $_POST['assunto'];
+$mensagem = $_POST['mensagem'];
 
-    $evangelhos = [
-        "Mateus" => 1,
-        "João" => 4,
-        "Lucas" => 3,
-        "Marcos" => 2,
-    ];
-    
-    foreach ($evangelhos as $evangelho => $ordem) {
-        echo "O evangelho de {$evangelho} é o livro número {$ordem} no Novo Testamento. <br>";
-    }
-    
-    echo "<br><br>";
-    
-    asort($evangelhos); // Ordenacao pelo value
-    foreach ($evangelhos as $evangelho => $ordem) {
-        echo "O evangelho de {$evangelho} é o livro número {$ordem} no Novo Testamento. <br>";
-    }
+$addEnvio = new MySQL($connData);
+
+$insertQuery = "INSERT INTO 
+                    envios(remetente_nome, remetente_email, destinatario_nome, destinatario_email, assunto, mensagem)
+                VALUES 
+                    ('{$remetenteNome}', '{$remetenteEmail}', '{$destinatarioNome}', '{$destinatarioEmail}', '{$assunto}', '{$mensagem}')";
+
+$addEnvioResult = $addEnvio->Query($insertQuery);
+
+if ($addEnvioResult === false) {
+    echo "Tivemos um problema no momento de inserir no banco de dados.";
 }
+
+/* Envio do Email */
+//.....
+
+// $nome = "Felipe";
+
+// if ($nome === "Felipe") {
+//     $response = [
+//         "nomeCompleto" => "Felipe Fernandes Campelo",
+//         "idade" => 35,
+//         "sexo" => "Masculino",
+//         "endereco" => [
+//             "logradouro" => "Rodovia Municipal dos Andradas"
+//         ],
+//     ];
+
+//     // for ($i = 0; $i <= 10; $i++) {
+//     //     echo $i;
+//     // }
+
+//     $evangelhos = [
+//         1 => "Mateus",
+//         4 => "João",
+//         3 => "Lucas",
+//         2 => "Marcos",
+//     ];
+    
+//     // foreach ($evangelhos as $evangelho => $ordem) {
+//     //     echo "O evangelho de {$evangelho} é o livro número {$ordem} no Novo Testamento. <br>";
+//     // }
+    
+//     // echo "<br><br>";
+    
+//     ksort($evangelhos); // Ordenacao pelo value
+//     // foreach ($evangelhos as $evangelho => $ordem) {
+//     //     echo "O evangelho de {$evangelho} é o livro número {$ordem} no Novo Testamento. <br>";
+//     // }
+
+//     for ($i = 1; $i <= count($evangelhos); $i++) {
+//         echo $i . " => " . $evangelhos[$i] . "<br>";
+//     }
+
+//     echo "<br><br>";
+
+//     // Checador booleano
+//     $checkError = false;
+//     $counter = count($evangelhos);
+
+//     // Iterable elements (array = Lista ou Matriz ... Objeto = Lista)
+//     if (count($evangelhos) < 4) {
+//         $checkError = true;
+//     }
+
+//     if ($checkError === true) {
+//         echo "aqui<br>";
+//         echo "Opa, o numero de Evangelhos da lista é {$counter}! Deveria ser 4.";
+//     } else {
+//         echo "nao, aqui<br>";
+//         echo "Excelente! O numero de Evangelhos da lista é {$counter}! De fato temos 4 Evangelhos no Novo Testamento.";
+//     }
+// }
